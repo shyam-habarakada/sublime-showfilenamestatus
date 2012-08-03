113ff4a4ffa29e02768c34b9785e6efd93d25c0d
shyam-habarakada <shyam.habarakada@gmail.com>
2012-08-03 15:06:03 -0700

    Bug fix

--- ShowFilenameInStatus.py
+++ ShowFilenameInStatus.py
@@ -6,9 +6,9 @@ class ShowFilenameInStatus(sublime_plugin.EventListener):
     def set_filename_status(self, view):
         filename = os.path.split(view.file_name())[1]
         if filename is None:
-            view.erase_status(status_key)
+            view.erase_status(self.status_key)
         else:
-            view.set_status(status_key, "File: " + filename)
+            view.set_status(self.status_key, "File: " + filename)
 
     def on_activated(self, view):
         self.set_filename_status(view)
